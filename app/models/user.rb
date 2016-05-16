c753d0897dc6f6148b0b0748c26267502cf21aef
Hideaki Oshima <b.i1993patriots@gmail.com>
2016-05-16 17:30:23 +0900

    modify validation of user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  validates_attachment_content_type :avatar, content_type: ["image/jpg","image/jpeg","image/png"]
+  
   validates :name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
 end
