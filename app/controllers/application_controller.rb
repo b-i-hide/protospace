bc8419c9cbfba4b184d53c1f9e4d521e973b1d48
Hideaki Oshima <b.i1993patriots@gmail.com>
2016-05-21 14:16:22 +0900

    set reirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :avatar
@@ -9,5 +10,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) << :profile
     devise_parameter_sanitizer.for(:sign_up) << :group
     devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
   end
 end
