cbdf344126159b2fd66e96cc42a390225ca7ebb1
Hideaki Oshima <b.i1993patriots@gmail.com>
2016-05-22 22:49:11 +0900

    edit strong parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :avatar
-    devise_parameter_sanitizer.for(:sign_up) << :works
-    devise_parameter_sanitizer.for(:sign_up) << :profile
-    devise_parameter_sanitizer.for(:sign_up) << :group
-    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:sign_up).push(:avatar, :works, :profile, :name)
+    devise_parameter_sanitizer.for(:account_update).push(:avatar, :works, :profile, :name)
   end
 
   def after_sign_out_path_for(resource)
