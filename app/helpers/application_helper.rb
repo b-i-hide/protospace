2b797915050645fe8c8533b0348413d6c693de15
Hideaki Oshima <b.i1993patriots@gmail.com>
2016-05-29 19:28:49 +0900

    remove unnecesary part

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,2 @@
 module ApplicationHelper
-  def devise_flash
-    if controller.devise_controller? && resource.errors.any?
-      flash.now[:error] = flash[:error].to_a.concat resource.errors.full_messages
-      flash.now[:error].uniq!
-    end
-  end
 end
