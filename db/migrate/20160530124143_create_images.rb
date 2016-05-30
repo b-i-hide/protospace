559a61153a35111bd22c4d638455aa78332528ed
Hideaki Oshima <b.i1993patriots@gmail.com>
2016-05-30 22:01:01 +0900

    create images table

---
+++ db/migrate/20160530124143_create_images.rb
@@ -0,0 +1,11 @@
+class CreateImages < ActiveRecord::Migration
+  def change
+    create_table :images do |t|
+      t.integer :prototype_id
+      t.text :image
+      t.integer :status
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160530091816) do
+ActiveRecord::Schema.define(version: 20160530124143) do
+
+  create_table "images", force: :cascade do |t|
+    t.integer  "prototype_id", limit: 4
+    t.text     "image",        limit: 65535
+    t.integer  "status",       limit: 4
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
   create_table "prototypes", force: :cascade do |t|
     t.string   "name",        limit: 255
