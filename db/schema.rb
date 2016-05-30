c47b04e5203daeaf9fc0324473de8218417df398
Hideaki Oshima <b.i1993patriots@gmail.com>
2016-05-30 18:31:13 +0900

    create prototrypes table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160520110827) do
+ActiveRecord::Schema.define(version: 20160530091816) do
+
+  create_table "prototypes", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.text     "concept",     limit: 65535
+    t.text     "chatch_copy", limit: 65535
+    t.integer  "likes_count", limit: 4
+    t.integer  "user_id",     limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
