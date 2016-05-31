9d3a6178ce086c0c50cb6a020732e4d28fe437a0
Hideaki Oshima <b.i1993patriots@gmail.com>
2016-05-31 13:13:52 +0900

    edit prototype controller

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -5,10 +5,21 @@ class PrototypesController < ApplicationController
   end
 
   def new
-
+    @prototype = Prototype.new
+    @prototype.images.build
   end
 
   def show
 
+  end
+
+  def create
+    Prototype.create(prototype_params)
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:name, :concept, :chatch_copy, images_attribute: [:id, :prototype_id, :status, :image]).merge(user_id: current_user.id)
   end
 end
