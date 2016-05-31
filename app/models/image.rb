b79c417a4111e5824c026e3dad007453d8edb69d
Hideaki Oshima <b.i1993patriots@gmail.com>
2016-05-31 13:15:03 +0900

    edit model of image and prototype

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,4 +3,5 @@ class Image < ActiveRecord::Base
   belongs_to :prototype
 
   enum image: { main: 0, sub: 1}
+  validates :prototype_id
 end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -3,6 +3,4 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :images
   accepts_nested_attributes_for :images
-
-  validates :user_id
 end
