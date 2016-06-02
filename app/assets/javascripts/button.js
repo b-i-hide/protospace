54598ca90b4abde8ea2d45766b4617964fd8b7ad
Hideaki Oshima <b.i1993patriots@gmail.com>
2016-06-02 15:54:00 +0900

    delete needless file

--- app/assets/javascripts/button.js
+++
@@ -1,7 +0,0 @@
-$("form").submit(function() {
-    var self = this;
-    $(":submit", self).prop("disabled", true);
-    setTimeout(function() {
-        $(":submit", self).prop("disabled", false);
-    }, 10000);
-});
