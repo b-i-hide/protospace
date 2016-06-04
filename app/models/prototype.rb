afa106309b94cda0ad33bb3fc4dcb23080822e67
Hideaki Oshima <b.i1993patriots@gmail.com>
2016-06-04 11:46:38 +0900

    edit prototype model

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,5 +2,12 @@ class Prototype < ActiveRecord::Base
 
   belongs_to :user
   has_many :images, dependent: :destroy
+  has_one :main_image, class_name: Image
   accepts_nested_attributes_for :images, reject_if: proc { |attributes| attributes['image'].blank? }, limit: 4
+  paginates_per 4
+
+  def posted_date
+    date = self.created_at.strftime("%b %d")
+    return date
+  end
 end
