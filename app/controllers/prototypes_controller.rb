ac8735d5023280d8f2da357810d7a5a833109700
Hideaki Oshima <b.i1993patriots@gmail.com>
2016-06-04 11:45:56 +0900

    modify prototype controller

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,9 +1,9 @@
 class PrototypesController < ApplicationController
 
   before_action :authenticate_user!, only: [:create, :new]
-  
+
   def index
-    @prototypes = Prototype.includes(:user).page(params[:page]).per(4)
+    @prototypes = Prototype.eager_load(:user, :images).page(params[:page])
   end
 
   def new
