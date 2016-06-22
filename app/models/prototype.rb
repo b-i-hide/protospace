50c7c33ae15cdb708de6863444ba1ea6d9b05fc3
Hideaki Oshima <hideakioshima@Hideaki-no-MacBook-Pro.local>
2016-06-23 00:04:22 +0900

    edit prototype model and controller

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -20,8 +20,8 @@ class Prototype < ActiveRecord::Base
     sub_images
   end
 
-  def like_user(user_id)
-    likes.find_by(user_id: user_id)
+  def like_user(user)
+    likes.find_by(user_id: user.id)
   end
 
 end
