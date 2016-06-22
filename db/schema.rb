35c37c3bd79cd7173e4b6488adabce97946316c9
Hideaki Oshima <hideakioshima@Hideaki-no-MacBook-Pro.local>
2016-06-23 00:07:51 +0900

    add likes_count to prototype table

--- db/schema.rb
+++ db/schema.rb
@@ -38,9 +38,9 @@ ActiveRecord::Schema.define(version: 20160612052355) do
     t.text     "concept",     limit: 65535
     t.text     "catch_copy",  limit: 65535
     t.integer  "user_id",     limit: 4
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.integer  "likes_count", limit: 4
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
+    t.integer  "likes_count", limit: 4,     default: 0
   end
 
   add_index "prototypes", ["user_id"], name: "index_prototypes_on_user_id", using: :btree
