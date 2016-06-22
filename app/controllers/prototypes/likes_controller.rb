3986346599eb23ca961b21c9f40036190fd76306
Hideaki Oshima <hideakioshima@Hideaki-no-MacBook-Pro.local>
2016-06-23 00:52:58 +0900

    edit likes controller

--- app/controllers/prototypes/likes_controller.rb
+++ app/controllers/prototypes/likes_controller.rb
@@ -3,13 +3,12 @@ class Prototypes::LikesController < ApplicationController
   before_action :set_prototype, only: %i(create destroy)
 
   def create
-    @like = @prototype.likes.create(user_id: current_user.id, prototype_id: params[:prototype_id])
+    @like = @prototype.likes.create(user_id: current_user.id)
     @prototype = Prototype.find(params[:prototype_id])
   end
 
   def destroy
-    like = @prototype.likes.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
-    like.destroy
+    @prototype.likes.find(params[:id]).destroy
     @prototype = Prototype.find(params[:prototype_id])
   end
 
