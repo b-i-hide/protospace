3c7d096658649fbea7f3b5376872e85946c68725
Hideaki Oshima <hideakioshima@Hideaki-no-MacBook-Pro.local>
2016-06-23 14:30:00 +0900

    edit prototype model

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -3,6 +3,7 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :images, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_one :main_image, class_name: "Image"
 
   accepts_nested_attributes_for :images, reject_if: proc { |attributes| attributes['image'].blank? }, limit: 4
