83293bb1d621682c18b750975c4bac298960d65e
Hideaki Oshima <hideakioshima@Hideaki-no-MacBook-Pro.local>
2016-06-25 01:45:08 +0900

    run migrate

--- db/migrate/20160623052017_create_comments.rb
+++ db/migrate/20160623052017_create_comments.rb
@@ -1,11 +1,11 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.text :commnet
+      t.text :comment
       t.references :user, index: true, foreign_key: true
       t.references :prototype, index: true, foreign_key: true
 
-      t.timestamps null: false
+      t.timestamps
     end
   end
 end
