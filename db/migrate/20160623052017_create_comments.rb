3aa34d3e7c8fd93affca772177d3dc8f07ee52da
Hideaki Oshima <hideakioshima@Hideaki-no-MacBook-Pro.local>
2016-06-25 01:40:25 +0900

    create view files for comments

--- db/migrate/20160623052017_create_comments.rb
+++ db/migrate/20160623052017_create_comments.rb
@@ -1,6 +1,9 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.text :commnet
+      t.references :user, index: true, foreign_key: true
+      t.references :prototype, index: true, foreign_key: true
 
       t.timestamps null: false
     end
