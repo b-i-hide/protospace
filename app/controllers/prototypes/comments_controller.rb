83e1e3a3e5c91549fe67868eab8287b2e28ea5fa
Hideaki Oshima <hideakioshima@Hideaki-no-MacBook-Pro.local>
2016-06-25 01:46:15 +0900

    create comments controller

---
+++ app/controllers/prototypes/comments_controller.rb
@@ -0,0 +1,19 @@
+class Prototypes::CommentsController < ApplicationController
+
+  before_action :set_prototype, only: %i(create)
+
+  def create
+    @comment = @prototype.comments.create(comment_params)
+  end
+
+  private
+
+  def comment_params
+    params.permit(:comment, :user_id, :prototype_id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:prototype_id])
+  end
+
+end
