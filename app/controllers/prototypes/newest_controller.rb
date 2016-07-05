d399d37705fdecb660084106f016b96375c293be
Hideaki Oshima <hideakioshima@Hideaki-no-MacBook-Pro.local>
2016-07-05 15:22:11 +0900

    edit newest controller

--- app/controllers/prototypes/newest_controller.rb
+++ app/controllers/prototypes/newest_controller.rb
@@ -1,2 +1,5 @@
 class Prototypes::NewestController < ApplicationController
+  def index
+    @prototypes = Prototype.includes(:user, :main_image).order('created_at DESC').page(params[:page]).per(9)
+  end
 end
