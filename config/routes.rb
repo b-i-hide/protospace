f7f441f46a98e156dbfc928cb5536608fab4c1bd
Hideaki Oshima <hideakioshima@Hideaki-no-MacBook-Pro.local>
2016-07-06 18:02:46 +0900

    edit routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
   resources :users, only: [:show, :edit, :update]
-
   resources :prototypes
   scope module: :prototypes do
     resources :likes, only: [:create, :destroy]
