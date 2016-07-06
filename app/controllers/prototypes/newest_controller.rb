c0ca26a335f59c775fd1379d4b98b7f0e2854d63
Hideaki Oshima <hideakioshima@Hideaki-no-MacBook-Pro.local>
2016-07-06 22:21:04 +0900

    edit newest controller

--- app/controllers/prototypes/newest_controller.rb
+++ app/controllers/prototypes/newest_controller.rb
@@ -1,6 +1,6 @@
 class Prototypes::NewestController < ApplicationController
   def index
-    @prototypes = Prototype.includes(:user, :main_image).order('created_at DESC').page(params[:page]).per(9)
+    @prototypes = Prototype.includes(:user, :main_image).order('created_at DESC').page(params[:page])
     @active = 'newest'
   end
 end
