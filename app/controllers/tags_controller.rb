8ec1caee6fb2cf7561bc4bd7681412a5ea681ab6
大島秀顕 <oshimahideaki@ooshimahideaki-no-MacBook-Pro.local>
2016-08-05 18:25:43 +0900

    edit controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,3 +1,10 @@
 class TagsController < ApplicationController
-  
+  def index
+    @tags = ActsAsTaggableOn::Tag.most_used
+  end
+
+  def show
+    @tag =  ActsAsTaggableOn::Tag.find(params[:id])
+    @prototype = Prototype.tagged_with(@tag.name)
+  end
 end
