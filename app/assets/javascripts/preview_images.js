5e5f04111021389b81da7a4535ba3e63ff1e59c0
b-i-hide <b.i1993patriots@gmail.com>
2016-08-24 20:19:55 +0900

    edit js and css

--- app/assets/javascripts/preview_images.js
+++ app/assets/javascripts/preview_images.js
@@ -1,9 +1,13 @@
-$(function () {
+$(document).ready(function () {
+    console.log('hoge');
     $('.js-image-uploader').each(function () {
+        console.log('piyo');
         // inputタグの取得
         var preview_image = $(this).find('input[type=file]');
         var preview_field = $(this);
+        console.log(preview_image);
         preview_image.change(function () {
+            console.log('change');
             var file = this.files[0];
             // fileの形式のチェック
             if(!file.type.match(/image/)) {
@@ -17,12 +21,11 @@ $(function () {
             };
             // ファイルの読み取りに成功した時
             filereader.onload = function () {
-               preview_field.css({
-                   'background-image': 'url(' + filereader.result + ')',
-                   'height': '100%',
-                   'background-size': 'cover'
-
-               });
+                preview_field.css({
+                    'background-image': 'url(' + filereader.result + ')',
+                    'height': '100%',
+                    'background-size': 'cover'
+                });
             };
             //　ファイルの読み取りの実行
             filereader.readAsDataURL(file);
