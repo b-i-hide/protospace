28f7844f7a9ae01054dab6f2d1fb90a25d52bb8c
b-i-hide <b.i1993patriots@gmail.com>
2016-08-25 00:56:23 +0900

    edit js

--- app/assets/javascripts/preview_images.js
+++ app/assets/javascripts/preview_images.js
@@ -1,13 +1,11 @@
+//prototypeのプレビュー
 $(document).ready(function () {
-    console.log('hoge');
     $('.js-image-uploader').each(function () {
-        console.log('piyo');
         // inputタグの取得
         var preview_image = $(this).find('input[type=file]');
         var preview_field = $(this);
         console.log(preview_image);
         preview_image.change(function () {
-            console.log('change');
             var file = this.files[0];
             // fileの形式のチェック
             if(!file.type.match(/image/)) {
@@ -24,6 +22,38 @@ $(document).ready(function () {
                 preview_field.css({
                     'background-image': 'url(' + filereader.result + ')',
                     'height': '100%',
+                    'background-size': 'cover'
+                });
+            };
+            //　ファイルの読み取りの実行
+            filereader.readAsDataURL(file);
+        });
+    });
+});
+
+//ユーザーのプレビュー
+$(document).ready(function () {
+    $('.js-avatar-uploader').each(function () {
+        // inputタグの取得
+        var preview_image = $(this).find('input[type=file]');
+        var preview_field = $(this);
+        console.log(preview_image);
+        preview_image.change(function () {
+            var file = this.files[0];
+            // fileの形式のチェック
+            if(!file.type.match(/image/)) {
+                alert('画像ファイルを選んでください');
+                return;
+            }
+            var filereader = new FileReader();
+            // ファイルの読み込みに失敗した時
+            filereader.onerror = function () {
+                alert('ファイルの読み込みに失敗しました')
+            };
+            // ファイルの読み取りに成功した時
+            filereader.onload = function () {
+                preview_field.css({
+                    'background-image': 'url(' + filereader.result + ')',
                     'background-size': 'cover'
                 });
             };
