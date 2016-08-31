c341326b5b79f40e81932f43f53740390706732b
b-i-hide <b.i1993patriots@gmail.com>
2016-08-31 18:08:32 +0900

    edit user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,47 +2,43 @@ require 'rails_helper'
 
 describe User do
    describe '#create' do
-     describe 'legal case' do
-       context 'create with completed data' do
-         it "should create user with name, email, password" do
-           user = create(:user)
-           expect(user).to be_valid
-         end
+     context 'with valid with all information' do
+       it "is valid with all information" do
+         user = create(:user)
+         expect(user).to be_valid
        end
      end
 
-     describe 'illegal case' do
-       context 'create with uncompleted data' do
-         it "is invalid without a name" do
-           user = build(:user, name: nil)
-           user.valid?
-           expect(user.errors[:name]).to include("can't be blank")
-         end
+     context 'without valid attributes' do
+       it "is invalid without a name" do
+         user = build(:user, name: nil)
+         user.valid?
+         expect(user.errors[:name]).to include("can't be blank")
+       end
 
-         it "should contain email" do
-           user = build(:user, email: nil)
-           user.valid?
-           expect(user.errors[:email]).to include("can't be blank")
-         end
+       it "is invalid without an email" do
+         user = build(:user, email: nil)
+         user.valid?
+         expect(user.errors[:email]).to include("can't be blank")
+       end
 
-         it "should contain password" do
-           user = build(:user, password: nil)
-           user.valid?
-           expect(user.errors[:password]).to include("can't be blank")
-         end
+       it "is invalid without a password" do
+         user = build(:user, password: nil)
+         user.valid?
+         expect(user.errors[:password]).to include("can't be blank")
+       end
 
-         it "should be invalid with duplicated email address" do
-           user = create(:user)
-           another_user = build(:user, email: user.email)
-           another_user.valid?
-           expect(another_user.errors[:email]).to include("has already been taken")
-         end
+       it "is invalid with a duplicated email address" do
+         user = create(:user)
+         another_user = build(:user, email: user.email)
+         another_user.valid?
+         expect(another_user.errors[:email]).to include("has already been taken")
+       end
 
-         it "should be contain more than 8 characters" do
-           user = build(:user, password: '0000')
-           user.valid?
-           expect(user.errors[:password][0]).to include('is too short')
-         end
+       it "is invalid with a password that has less than 7 characters" do
+         user = build(:user, password: '0000')
+         user.valid?
+         expect(user.errors[:password][0]).to include('is too short')
        end
      end
   end
