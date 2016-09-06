6211ad1c59c1a7db09d0ea327a11b8cc0fb5f82c
b-i-hide <b.i1993patriots@gmail.com>
2016-09-06 17:58:17 +0900

    edit factories

--- spec/factories/prototypes.rb
+++ spec/factories/prototypes.rb
@@ -1,9 +1,13 @@
 FactoryGirl.define do
   factory :prototype do
-    name { Faker::Commerce.product_name }
+    name 'test'
     concept { Faker::Lorem.sentence }
     catch_copy { Faker::Lorem.word }
     created_at { Faker::Time.between(2.days.ago, Time.now) }
     user
+
+    after(:create) do |prototype|
+      create(:image, :main_image, prototype: prototype)
+    end
   end
 end
