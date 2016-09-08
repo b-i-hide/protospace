4ee40bc899bbe8afcb74358f88ed945679801c02
b-i-hide <b.i1993patriots@gmail.com>
2016-09-09 01:23:36 +0900

    set capybara driver

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,7 @@ require 'support/controller_macros'
 
 # Setting for Capybara
 require 'capybara/poltergeist'
-Capybara.javascript_driver = :poltergeist
+Capybara.javascript_driver = :selenium
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
