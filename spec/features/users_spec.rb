cfe6971583c36feb8195c216cf6fa812b80848eb
b-i-hide <b.i1993patriots@gmail.com>
2016-09-12 17:17:23 +0900

    riferctering feature spec

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,20 +1,38 @@
 require 'rails_helper'
 
 feature 'User management' do
-  subject(:user){ create(:user) }
+  let(:registered_user){ create(:user) }
+  let(:new_user){ build(:user) }
+  let(:prototype){ build(:prototype) }
 
   scenario 'sign up' do
-    sign_up
+    visit root_path
+    click_link 'Get Started'
+    click_link 'Sign up now'
+    fill_in 'Name', with: new_user.name
+    fill_in 'Password', with: new_user.password
+    fill_in 'E-Mail', with: new_user.email
+    click_on 'Sign up'
     expect(page).to have_content 'Welcome! You have signed up successfully.'
   end
 
   scenario 'sign in' do
-    sign_in
+    visit root_path
+    click_link 'Get Started'
+    fill_in 'Email address', with: registered_user.email
+    fill_in 'Password', with: registered_user.password
+    click_button 'Sign in'
     expect(page).to have_content 'Signed in successfully.'
   end
 
   scenario 'post prototype', js: true do
-    post_prototype
+    sign_up
+    click_on 'New Proto'
+    fill_in 'Title', with: prototype.name
+    attach_file 'prototype[images_attributes][0][image]', "#{Rails.root}/spec/fixtures/img/sample.jpg", visible: false
+    fill_in 'Catch Copy', with: prototype.catch_copy
+    fill_in 'Concept', with: prototype.concept
+    click_button 'Publish'
     expect(page).to have_content 'Your prototype was successfully posted'
   end
 end
--- spec/support/feature_macros.rb
+++ spec/support/feature_macros.rb
@@ -3,27 +3,9 @@ module FeatureMacros
     visit root_path
     click_link 'Get Started'
     click_link 'Sign up now'
-    fill_in 'Name', with: 'hoge'
-    fill_in 'Password', with: '00000000'
-    fill_in 'E-Mail', with: 'hoge@gmail.com'
+    fill_in 'Name', with: new_user.name
+    fill_in 'Password', with: new_user.password
+    fill_in 'E-Mail', with: new_user.email
     click_on 'Sign up'
-  end
-
-  def sign_in
-    visit root_path
-    click_link 'Get Started'
-    fill_in 'Email address', with: subject.email
-    fill_in 'Password', with: subject.password
-    click_button 'Sign in'
-  end
-
-  def post_prototype
-    sign_up
-    click_on 'New Proto'
-    fill_in 'Title', with: 'hoge'
-    attach_file 'prototype[images_attributes][0][image]', "#{Rails.root}/spec/fixtures/img/sample.jpg", visible: false
-    fill_in 'Catch Copy', with: 'hoge'
-    fill_in 'Concept', with: 'hoge'
-    click_button 'Publish'
   end
 end
